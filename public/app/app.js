fc8519cb00b43ea4444b5eb3aac9c9654f050f6a
Slavi Nikolov <admin@design-fb.com>
2014-09-21 17:43:54 +0300

    Settedup MEAN

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,14 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngResource', 'ngRoute']);
+
+app.config(function($routeProvider, $locationProvider){
+    //$locationProvider.html5Mode(true);
+
+    $routeProvider
+        .when('/', {
+            templateUrl: '/partials/home',
+            controller: 'MainController'
+        })
+});
 
 app.controller('MainController', function($scope){
     $scope.hello = 'Hi From Angular';
--- server.js
+++ server.js
@@ -1,33 +1,20 @@
-var express = require('express');
-var stylus = require('stylus');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-
-var evn = process.env.NODE_ENV || 'development';
-
-var app = express();
+var express = require('express'),
+    stylus = require('stylus'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    mongoose = require('mongoose'),
+    port = 3030,
+    evn = process.env.NODE_ENV || 'development',
+    app = express();
 
 app.set('view engine', 'jade');
-
 app.set('views', __dirname + '/server/views');
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 app.use(bodyParser.json());
 app.use(morgan('combined'));
-
-app.get('*', function(req, res) {
-    res.render('index');
-});
-
-var port = 3030;
-
-app.listen(port);
-
-console.log('Server running on port ' + port);
-
 app.use(stylus.middleware(
     {
         src: __dirname + '/public',
@@ -35,6 +22,49 @@ app.use(stylus.middleware(
            return stylus(str).set('filename', path);
         }
     }
-))
+));
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname + '/public'))
+mongoose.connect('mongodb://localhost/manualapp');
+var db = mongoose.connection;
+
+db.once('open', function(err){
+    if(err) {
+        console.log('Database could not be opened: ' + err);
+        return;
+    }
+    console.log('Database up and running...')
+});
+
+db.on('error', function (err) {
+    console.log('Database error: ' + err);
+});
+
+var messageSchema = mongoose.Schema({
+    message: String
+})
+
+var Message = mongoose.model('Message', messageSchema);
+var messageFromDatabase;
+
+Message.remove({}).exec(function(err){
+    if(err){
+        console.log('Message could not be cleared: ' + err);
+        return;
+    }
+    Message.create({message: 'Hi from Mongoose'})
+        .then(function(model){
+            messageFromDatabase = model.message;
+        });
+});
+
+app.get('/partials/:partialName', function (req, res) {
+    res.render('partials/' + req.params.partialName)
+});
+app.get('*', function(req, res) {
+    res.render('index', {message: messageFromDatabase});
+});
+
+app.listen(port);
+
+console.log('Server running on port ' + port);
