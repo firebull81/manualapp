ae17073a98dd84fd617cf7b9c8ea9670c3cc90d1
Slavi Nikolov <admin@design-fb.com>
2014-09-21 22:45:29 +0300

    Finishing UI

--- public/app/app.js
+++ public/app/app.js
@@ -1,15 +1,11 @@
 var app = angular.module('app', ['ngResource', 'ngRoute']);
 
 app.config(function($routeProvider, $locationProvider){
-    //$locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
 
     $routeProvider
         .when('/', {
             templateUrl: '/partials/home',
             controller: 'MainController'
         })
-});
-
-app.controller('MainController', function($scope){
-    $scope.hello = 'Hi From Angular';
 });
---
+++ public/app/main/mainCtrl.js
@@ -0,0 +1,21 @@
+'use strict'
+
+app.controller('MainController', function($scope){
+  $scope.courses = [
+  {name: 'Course #1', featured: true, published: new Date('10/5/2013')},
+  {name: 'Course #2', featured: true, published: new Date('10/6/2013')},
+  {name: 'Course #3', featured: false, published: new Date('10/7/2013')},
+  {name: 'Course #4', featured: false, published: new Date('10/8/2013')},
+  {name: 'Course #5', featured: true, published: new Date('10/9/2013')},
+  {name: 'Course #6', featured: true, published: new Date('10/10/2013')},
+  {name: 'Course #7', featured: true, published: new Date('10/11/2013')},
+  {name: 'Course #8', featured: true, published: new Date('10/12/2013')},
+  {name: 'Course #9', featured: true, published: new Date('11/5/2014')},
+  {name: 'Course #10', featured: true, published: new Date('12/5/2013')},
+  {name: 'Course #11', featured: false, published: new Date('13/5/2013')},
+  {name: 'Course #12', featured: false, published: new Date('14/5/2012')},
+  {name: 'Course #13', featured: true, published: new Date('15/5/2013')},
+  {name: 'Course #14', featured: false, published: new Date('16/5/2013')},
+  {name: 'Course #15', featured: true, published: new Date('17/5/2013')}
+  ]
+});
