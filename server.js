2fa2e52d88425b1d97c4248d97c1c76178c324ff
Slavi Nikolov <admin@design-fb.com>
2014-09-22 17:04:57 +0300

    Config Server to use configuration

--- server.js
+++ server.js
@@ -1,57 +1,11 @@
 var express = require('express'),
-    stylus = require('stylus'),
-    bodyParser = require('body-parser'),
-    morgan = require('morgan'),
-    mongoose = require('mongoose'),
     env = process.env.NODE_ENV || 'development',
-    port = process.env.PORT || 3030,
-    app = express();
+    app = express(),
+    config = require('./server/config/config')[env];
 
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/server/views');
+require('./server/config/express')(app, config);
+require('./server/config/mongoose')(config);
+require('./server/config/routes')(app);
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-app.use(morgan('combined'));
-app.use(stylus.middleware(
-    {
-        src: __dirname + '/public',
-        compile: function(str, path) {
-           return stylus(str).set('filename', path);
-        }
-    }
-));
-app.use(express.static(__dirname + '/public'));
-
-if (env == 'development') {
-    mongoose.connect('mongodb://localhost/manualapp');
-}
-else {
-    mongoose.connect('mongodb://ManualApp:CrossBull81@ds033400.mongolab.com:33400/manualapp');
-}
-var db = mongoose.connection;
-
-db.once('open', function(err){
-    if(err) {
-        console.log('Database could not be opened: ' + err);
-        return;
-    }
-    console.log('Database up and running...')
-});
-
-db.on('error', function (err) {
-    console.log('Database error: ' + err);
-});
-
-app.get('/partials/:partialArea/:partialName', function (req, res) {
-    res.render('partials/' + req.params.partialArea + '/' + req.params.partialName)
-});
-app.get('*', function(req, res) {
-    res.render('index');
-});
-
-app.listen(port);
-
-console.log('Server running on port ' + port);
+app.listen(config.port);
+console.log('Server running on port ' + config.port);
